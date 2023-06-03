24eb2a5ceca70881adf84cca90083960717a717e
Madhumitha <gmadhumitha98@gmail.com>
2023-06-03 16:24:09 +0530

    updates

--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import App from "../App";
 import RegisterComponent from "../component/Register/registerComponent";
 
 function RouterApp() {
     return (
         <Routes>
-            <Route path="/" element={<App />} />
             <Route path="/signin" element={<RegisterComponent />} />
         </Routes>
     )
--- src/component/Register/registerComponent.tsx
+++ src/component/Register/registerComponent.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './registerComponent.scss'
 export default class RegisterComponent extends React.Component {
     render(): React.ReactNode {
         return (
-            <div>
-                Register Component
+            <div className='container'>
+                <h1>Log In</h1>
+                <div className='reg-email'>
+                    <label> Email</label>
+                    <input type='email' placeholder='Email'/>
+                </div>
+                <div className='reg-password'>
+                    <label>Password</label>
+                    <input type='password' placeholder='Enter your password'  />
+                </div>
+                <footer>
+                    <button className='btn-login'>
+                        Login
+                    </button>
+                </footer>
             </div>
         )
     }
