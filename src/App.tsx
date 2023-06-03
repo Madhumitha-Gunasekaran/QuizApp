a7898940ff30c344255a3074dffb63934f1c4399
Madhumitha <gmadhumitha98@gmail.com>
2023-06-03 10:41:30 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import AppRoutes from './Router/Routes';
+import { Router } from 'react-router-dom'
+import React from 'react';
+
+function App() {
+  return (
+    <div>Hello App</div>
+
+  )
+}
+
+export default App;
---
+++ src/component/Register/registerComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class RegisterComponent extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <div>
+                Register Component
+            </div>
+        )
+    }
+}
